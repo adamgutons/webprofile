8854df10a83d51e1129b17e8537e78d3b019a7d2
Adam Gutonski <adamgutons@gmail.com>
2018-11-05 22:31:30 -0500

    more js edits

--- js/main.js
+++ js/main.js
@@ -19,7 +19,32 @@ $(document).ready(function() {
 		text.fontSize = 20;
 		text.content = 'click and drag mouse to draw';
 	};
-	
+
+	function getRandomInt(min, max){
+  		min = Math.ceil(min);
+  		max = Math.floor(max);
+  		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+ 	document.getElementById('clear').onclick = function(){
+
+        paper.project.clear(); // paper has project property which is all layers in project, so everything on canvas i guess.  clears them 
+        drawCircle(); //so far i just call this again to redraw the cirlce...
+        console.log('layers cleared');
+        document.getElementById("alerts").innerHTML = 'Sketch Cleared';
+
+
+    }; 
+
+    document.getElementById('clear').ontouch = function(){
+
+        paper.project.clear(); // paper has project property which is all layers in project, so everything on canvas i guess.  clears them 
+        drawCircle(); //so far i just call this again to redraw the cirlce...
+        console.log('layers cleared');
+        document.getElementById("alerts").innerHTML = 'Sketch Cleared';
+
+
+    }; 
 	 //create path
 	var tool = new Tool(); //create tool
 	// This function is called whenever the user releases the mouse button AFTER a click
@@ -28,10 +53,14 @@ $(document).ready(function() {
 	tool.onMouseDown = function(event) { 
 		console.log('you clicked');
 		myPath = new Path(); //create a new Path object each time 
-		myPath.strokeColor = 'black'; // make the line black each time
+		myPath.strokeColor = "#ff0000";
+		myPath.strokeColor.hue = getRandomInt(0, 360); // make the line black each time
+		myPath.strokeWidth = getRandomInt(0.5,15);
+
 		//myPath.add(event.downPoint); // uses the onMouseUp event handler only and accesses event.downPoint = 
 									 //The position of the mouse in project coordinates when the mouse button was last clicked.
 		myPath.add(event.point);	 //point = coords where event fired, in this case user releasing mouse button 
+		document.getElementById("alerts").innerHTML = '';//click to draw a new line and clears the alert text
 	}
 
 	tool.onMouseDrag = function(event) {
@@ -39,15 +68,9 @@ $(document).ready(function() {
 	}
 	
 
-	 document.getElementById('clear').onclick = function(){
+	
 
-            paper.project.clear(); // paper has project property which is all layers in project, so everything on canvas i guess.  clears them 
-            drawCircle(); //so far i just call this again to redraw the cirlce...
-            console.log('layers cleared')
-
-        }; 
-
- 
+   
 
 
 	//tells paper.js to draw on screen
